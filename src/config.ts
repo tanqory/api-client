0db26e39fd1096736125c8ae644d60728d52b4aa
mwongsakorn <wongsakorn.m@zenithme.co.th>
2025-08-06 16:32:22 +0700

    * fix(config): update version in package-lock.json and package.json * fix(config): remove setting apiUrl and siteId in setConfig function

--- src/config.ts
+++ src/config.ts
@@ -16,14 +16,6 @@ export const setConfig = (options: TanqoryInitOptions): void => {
     config.refreshToken = options.refreshToken;
     console.log("Tanqory Library: Refresh Token has been set.");
   }
-  if (options && options.apiUrl) {
-    config.apiUrl = options.apiUrl;
-    console.log(`Tanqory Library: API URL has been set to ${config.apiUrl}`);
-  }
-  if (options && options.siteId) {
-    config.siteId = options.siteId;
-    console.log(`Tanqory Library: Site ID has been set to ${config.siteId}`);
-  }
 };
 
 export const getConfig = (): TanqoryConfig => {
@@ -31,7 +23,7 @@ export const getConfig = (): TanqoryConfig => {
 };
 
 export const checkConfig = (): void => {
-  if (!config.accessToken || !config.siteId) {
+  if (!config.accessToken) {
     throw new Error("Tanqory Library: Access Token or Site ID is not set. Please call Tanqory.init() with both values first.");
   }
 };
